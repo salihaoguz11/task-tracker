5061fb4f75f997e25c75d10acd0a407423bb910e
Saliha <salihaoguz2011@gmail.com>
2023-02-23 11:41:37 +0000

    show task bar

--- src/components/addTask/AddTask.jsx
+++ src/components/addTask/AddTask.jsx
@@ -1,17 +1,38 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useState } from "react";
+import axios from "axios";
 
 function AddTask() {
+  const [task, setTask] = useState("");
+  const [date, setDate] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTask = { task, date };
+    addNewTask(newTask);
+    setDate("");
+    setTask("");
+  };
+  const addNewTask = async (newTask) => {
+    const url = "https://63f7293b833c7c9c607dd928.mockapi.io/api/tasks";
+    try {
+      await axios.post(url, newTask);
+    } catch (error) {}
+  };
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Task</Form.Label>
-        <Form.Control type="text" placeholder="Enter Task" />
+        <Form.Control
+          type="text"
+          placeholder="Enter Task"
+          onChange={(e) => setTask(e.target.value)}
+        />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Date</Form.Label>
-        <Form.Control type="date" />
+        <Form.Control type="date" onChange={(e) => setDate(e.target.value)} />
       </Form.Group>
 
       <div className="text-center">
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,39 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import Button from "react-bootstrap/esm/Button";
 import AddTask from "../components/addTask/AddTask";
 import TaskList from "../components/taskList/TaskList";
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [text, setText] = useState("Show Task Bar");
+  const [task, setTask] = useState([]);
+  const url = "https://63f7293b833c7c9c607dd928.mockapi.io/api/tasks";
+  const toggle = () => {
+    setIsOpen(!isOpen);
+    const buttonText = isOpen ? "Show Task Bar" : "Hide Task Bar";
+    setText(buttonText);
+  };
+  const getTask = async () => {
+    const { data } = await axios(url);
+    setTask(data);
+  };
+  useEffect(() => {
+    getTask();
+  }, []);
   return (
-    <div>
-      <AddTask />
+    <div className="mt-4 d-flex  justify-content-center flex-column">
+      <Button
+        variant="danger"
+        onClick={() => {
+          toggle();
+        }}
+      >
+        {text}
+      </Button>
+      {isOpen && <AddTask />}
+
       <TaskList />
     </div>
   );
