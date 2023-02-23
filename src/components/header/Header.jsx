b62275dcac45b17f3090ab049a47f25241afefe9
Saliha <salihaoguz2011@gmail.com>
2023-02-23 09:32:38 +0000

    header done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,11 @@
+import TaskPng from "../../assets/TaskPng.jsx";
 const Header = () => {
-  return <div></div>;
+  return (
+    <div className="d-flex justify-content-evenly align-items-center bg-danger">
+      <TaskPng />
+      <h1>TASK TRACKER</h1>
+    </div>
+  );
 };
 
 export default Header;
